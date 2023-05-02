b461ff3cde78d8ab8c41fc693a009044449c2899
DavidGGUPM <114806494+DavidGGUPM@users.noreply.github.com>
2023-05-02 16:53:01 +0200

    Update chart-device-data.js

--- public/js/chart-device-data.js
+++ public/js/chart-device-data.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
-$(document).ready(() => {
+/* $(document).ready(() => {
   // if deployed to a site supporting SSL, use wss://
   const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
   const webSocket = new WebSocket(protocol + location.host);
@@ -171,6 +171,217 @@ $(document).ready(() => {
       }
 
       myLineChart.update();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+}); */
+
+$(document).ready(() => {
+  // if deployed to a site supporting SSL, use wss://
+  const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
+  const webSocket = new WebSocket(protocol + location.host);
+
+  // A class for holding the last N points of telemetry for a device
+  // FINAL ASSIGNMENT: we do not need any array, as we only need to print the last value received
+  // FINAL ASSIGNMENT: lat & long in the map, and distance & speed in the labels
+  class DeviceData {
+    constructor(deviceId) {
+      this.deviceId = deviceId;
+      /*this.maxLen = 50;
+      this.timeData = new Array(this.maxLen);
+      this.temperatureData = new Array(this.maxLen);
+      this.humidityData = new Array(this.maxLen);*/
+      this.timeData = null;
+      this.distance = null;
+      this.speed = null;
+      this.latitude = 40.388461795407444;
+      this.longitude = -3.6298768772729666;
+
+    }
+
+    // FINAL ASSIGNMENT: this method add the elements to the arrays. In our case, it is easier, as we do 
+    // not need to append elements, only overwrite them
+    /*addData(time, temperature, humidity) {
+      this.timeData.push(time);
+      this.temperatureData.push(temperature);
+      this.humidityData.push(humidity || null);
+
+      if (this.timeData.length > this.maxLen) {
+        this.timeData.shift();
+        this.temperatureData.shift();
+        this.humidityData.shift();
+      }
+    }*/
+    addData(t, dist, sp, lat, long) {
+      this.time = t;
+      this.distance = dist;
+      this.speed = sp;
+      this.latitude = lat;
+      this.longitude = long;
+    }
+  }
+
+  // All the devices in the list (those that have been sending telemetry)
+  // FINAL ASSIGNMENT: it is the same for our case, so we do not need to change anything here
+  class TrackedDevices {
+    constructor() {
+      this.devices = [];
+    }
+
+    // Find a device based on its Id
+    findDevice(deviceId) {
+      for (let i = 0; i < this.devices.length; ++i) {
+        if (this.devices[i].deviceId === deviceId) {
+          return this.devices[i];
+        }
+      }
+
+      return undefined;
+    }
+
+    getDevicesCount() {
+      return this.devices.length;
+    }
+  }
+
+  const trackedDevices = new TrackedDevices();
+
+  // Define the chart axes
+  // FINAL ASSIGNMENT: we do not need any chart, only the map and two labels for the distance and speed
+  /*const chartData = {
+    datasets: [
+      {
+        fill: false,
+        label: 'Temperature',
+        yAxisID: 'Temperature',
+        borderColor: 'rgba(255, 204, 0, 1)',
+        pointBoarderColor: 'rgba(255, 204, 0, 1)',
+        backgroundColor: 'rgba(255, 204, 0, 0.4)',
+        pointHoverBackgroundColor: 'rgba(255, 204, 0, 1)',
+        pointHoverBorderColor: 'rgba(255, 204, 0, 1)',
+        spanGaps: true,
+      },
+      {
+        fill: false,
+        label: 'Humidity',
+        yAxisID: 'Humidity',
+        borderColor: 'rgba(24, 120, 240, 1)',
+        pointBoarderColor: 'rgba(24, 120, 240, 1)',
+        backgroundColor: 'rgba(24, 120, 240, 0.4)',
+        pointHoverBackgroundColor: 'rgba(24, 120, 240, 1)',
+        pointHoverBorderColor: 'rgba(24, 120, 240, 1)',
+        spanGaps: true,
+      }
+    ]
+  };
+
+  const chartOptions = {
+    scales: {
+      yAxes: [{
+        id: 'Temperature',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Temperature (ºC)',
+          display: true,
+        },
+        position: 'left',
+      },
+      {
+        id: 'Humidity',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Humidity (%)',
+          display: true,
+        },
+        position: 'right',
+      }]
+    }
+  };*/
+
+  // Get the context of the canvas element we want to select
+  // FINAL ASSIGNMENT: as we do not use chart, we do not need this part of code
+  // FINAL ASSIGNMENT: only obtain the ids of the HTML file and update with the current values
+  /*const ctx = document.getElementById('iotChart').getContext('2d');
+  const myLineChart = new Chart(
+    ctx,
+    {
+      type: 'line',
+      data: chartData,
+      options: chartOptions,
+    });*/
+  
+
+  // Manage a list of devices in the UI, and update which device data the chart is showing
+  // based on selection
+  // FINAL ASSIGNMENT: let this part as it is, as we need to manage different devices
+  let needsAutoSelect = true;
+  const deviceCount = document.getElementById('deviceCount');
+  const listOfDevices = document.getElementById('listOfDevices');
+
+  const mapLayout = document.getElementById('map');
+  const distanceLabel = document.getElementById('distance');
+  const speedLabel = document.getElementById('speed');
+
+  function OnSelectionChange() {
+    const device = trackedDevices.findDevice(listOfDevices[listOfDevices.selectedIndex].text);
+    /*chartData.labels = device.timeData;
+    chartData.datasets[0].data = device.temperatureData;
+    chartData.datasets[1].data = device.humidityData;
+    myLineChart.update();*/
+    distanceLabel = device.distance;
+    speedLabel = device.speed;
+  }
+  listOfDevices.addEventListener('change', OnSelectionChange, false);
+
+  // When a web socket message arrives:
+  // 1. Unpack it
+  // 2. Validate it has date/time and temperature
+  // 3. Find or create a cached device to hold the telemetry data
+  // 4. Append the telemetry data
+  // 5. Update the chart UI
+  // FINAL ASSIGNMENT: similar to that code: unpack the code, validate it has the 4 variables (lat, long, distance and speed),
+  // FINAL ASSIGNMENT: hold the telemetry data, append it and update the HTML elements (map and labels)
+  webSocket.onmessage = function onMessage(message) {
+    try {
+      const messageData = JSON.parse(message.data);
+      console.log(messageData);
+
+      // time and either temperature or humidity are required
+      /*if (!messageData.MessageDate || (!messageData.IotData.temperature && !messageData.IotData.humidity)) {
+        return;
+      }*/
+      if (!messageData.MessageDate || (!messageData.IotData.distance && !messageData.IotData.speed && !messageData.IotData.latitude && !messageData.IotData.longitude)) {
+        return;
+      }
+
+      // find or add device to list of tracked devices
+      const existingDeviceData = trackedDevices.findDevice(messageData.DeviceId);
+
+      if (existingDeviceData) {
+        existingDeviceData.addData(messageData.MessageDate, messageData.IotData.distance, messageData.IotData.speed, messageData.IotData.latitude, messageData.IotData.longitude);
+      } else {
+        const newDeviceData = new DeviceData(messageData.DeviceId);
+        trackedDevices.devices.push(newDeviceData);
+        const numDevices = trackedDevices.getDevicesCount();
+        deviceCount.innerText = numDevices === 1 ? `${numDevices} device` : `${numDevices} devices`;
+        newDeviceData.addData(messageData.MessageDate, messageData.IotData.distance, messageData.IotData.speed, messageData.IotData.latitude, messageData.IotData.longitude);
+
+        // add device to the UI list
+        const node = document.createElement('option');
+        const nodeText = document.createTextNode(messageData.DeviceId);
+        node.appendChild(nodeText);
+        listOfDevices.appendChild(node);
+
+        // if this is the first device being discovered, auto-select it
+        if (needsAutoSelect) {
+          needsAutoSelect = false;
+          listOfDevices.selectedIndex = 0;
+          OnSelectionChange();
+        }
+      }
+
+      //myLineChart.update();
     } catch (err) {
       console.error(err);
     }
